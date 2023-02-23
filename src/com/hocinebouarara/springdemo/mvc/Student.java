b8e2f6b2abd6563d94a5b8813b75fc280460656a
hocinebouarara <hocine@univ-tiaret.dz>
2023-02-23 07:37:23 +0100

    using the CheckBoxes in spring mvc

--- src/com/hocinebouarara/springdemo/mvc/Student.java
+++ src/com/hocinebouarara/springdemo/mvc/Student.java
@@ -14,6 +14,8 @@ public class Student {
 	
 	private String favoriteLanguage;
 	
+	private String[] operatingSystems;
+	
 	public Student () {
 		// populate country option: used ISO country code
 		countryOptions = new LinkedHashMap<>();
@@ -62,6 +64,13 @@ public class Student {
 	public void setFavoriteLanguage(String favoriteLanguage) {
 		this.favoriteLanguage = favoriteLanguage;
 	}
-	
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
 	
 }
--- webContent/WEB-INF/view/student-confirmation.jsp
+++ webContent/WEB-INF/view/student-confirmation.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE>
 <html>
 	<head>
@@ -14,6 +15,20 @@
 		<br> <br>
 		
 		Favorite Language: ${student.favoriteLanguage }
+		
+		<br> <br>
+		
+		Operating Systems:
+		
+		<ul>
+		
+			<c:forEach var="temp" items="${student.operatingSystems}">
+			
+				<li> ${temp } </li>
+		
+			</c:forEach>
+			
+		</ul>
 		
 	</body>
 </html>
